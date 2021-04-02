af1aca209e840b89f83907bafa0839bc647dab19
sarvangdave_crest <sarvang.dave@crestdatasys.com>
2021-04-02 13:51:02 +0530

    Title update

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">Fibo Calculator</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
