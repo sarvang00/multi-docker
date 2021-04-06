1f2f89aefef91998d10e52ffcb1335771f0455d8
sarvangdave_crest <sarvang.dave@crestdatasys.com>
2021-04-06 15:09:30 +0530

    App version update

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Fibo Calculator</h1>
+          <h1 className="App-title">Fibo Calculator v.2</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
